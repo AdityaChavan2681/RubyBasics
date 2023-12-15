7d645f2616d5df3c99b830e6d0fab06c3570d8f2
AdityaChavan2681 <entp847@gmail.com>
2023-12-15 19:01:49 +0530

    getter setter 2

--- OOP projects/student.rb
+++ OOP projects/student.rb
@@ -1,22 +1,32 @@
 class Student # class/object definition
+  attr_accessor :first_name, :last_name, :email, :username
+  
   @first_name #class instance/instance variable
   @last_name
   @email
   @username
   @password
 
-  # setter
-  def first_name(name)
-    @first_name = name
-  end
+  # # setter
+  # def first_name(name)
+  #   @first_name = name
+  # end
 
-  # getter
+  # # getter
+  # def first_name
+  #   @first_name
+  # end
+
   def to_s
     "First name: #{@first_name}"
   end
-end
-
-john = Student.new
-puts john
-john.first_name("John")
-puts john
+  
+  john = Student.new
+  john.first_name = "John"
+  john.last_name = "Doe"
+  john.email = "john@doe.com"
+  puts john.first_name
+  puts john.last_name
+  puts john.email
+  puts john.username
+end 
