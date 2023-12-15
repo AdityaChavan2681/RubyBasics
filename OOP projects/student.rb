e61c9987319edda5ddfd472a0e1e5e0e8904b664
AdityaChavan2681 <entp847@gmail.com>
2023-12-15 17:03:21 +0530

    intro to oop

--- OOP projects/student.rb
+++ OOP projects/student.rb
@@ -1,32 +1,17 @@
-require_relative "bcrypt_example"
+class Student # class/object definition
+  @first_name #class instance/instance variable
+  @last_name
+  @email
+  @username
+  @password
 
+  def to_stringg
+    "First name:#{@first_name}"
 
-class Student
-  include Crud
-  attr_accessor :first_name, :last_name, :email, :username, :password
-
-  def initialize(first_name, last_name, username, email, password)
-    @first_name = first_name
-    @last_name = last_name
-    @username = username
-    @email = email
-    @password = password
   end
-
-  def to_s
-    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, Email: #{@email}"
-  end
-
 end
 
-  johndoe = Student.new("john","doe","john1","john@example.com","password1")
-
-  henryford = Student.new("henry","ford","henry1","henry@example.com","password2")
-
-  puts johndoe
-
-  puts henryford
-
-  hashed_password = johndoe.create_hash_digest(johndoe.password)
-
-  puts hashed_password
+john = Student.new
+puts john
+# john.first_name = "John"
+# puts john
